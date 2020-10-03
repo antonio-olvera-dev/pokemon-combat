26b854c09c0504092641018ab8f7a4d7a8a570d1
PC-ANTONIO\toni_ <antonioolvera1995@gmail.com>
2020-10-03 20:13:52 +0200

    Commit-6

---
+++ js/pokemon.js
@@ -0,0 +1,33 @@
+"use strict";
+
+var venusaur = {
+    nombre: "Venusaur",
+    tipo: ['Planta', 'Veneno'],
+    vida: 270,
+    ataque: 169,
+    defensa: 171
+}
+
+var charizard = {
+    nombre: "Charizard",
+    tipo: ['Fuego', 'Volador'],
+    vida: 266,
+    ataque: 173,
+    defensa: 161
+}
+
+var blastoise = {
+    nombre: "Blastoise",
+    tipo: ['Agua'],
+    vida: 268,
+    ataque: 171,
+    defensa: 205
+}
+
+var raichu = {
+    nombre: "Raichu",
+    tipo: ['Eléctrico'],
+    vida: 266,
+    ataque: 173,
+    defensa: 161
+}
